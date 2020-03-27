544dd75f0ddd6a0eb955d0103d4cb73696193b2d
sishubjoshi <shisub1999@gmail.com>
2020-03-27 12:17:49 +0530

    initial commit:zap:

---
+++ public/js/main.js
@@ -0,0 +1,36 @@
+const socket = io();
+
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+chatForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const msg = e.target.elements.msg.value;
+
+	console.log(msg);
+	socket.emit('chatMessage', msg);
+
+	e.target.elements.msg.value = '';
+	e.target.elements.msg.focus();
+});
+
+socket.on('message', (msg) => {
+	// console.log(msg);
+	displayMessage(msg);
+	chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+function displayMessage(msg) {
+	const div = document.createElement('div');
+	div.classList.add('message');
+
+	div.innerHTML = `
+    <p class="meta">username <span>9:26 am</span></p>
+    <p class="text">
+        ${msg}
+    </p>`;
+
+	chatMessages.appendChild(div);
+
+	// document.querySelector('.chat-mess')
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+	// console.log('connection established');
+	socket.broadcast.emit('message', 'A user has joined..');
+
+	socket.on('chatMessage', (msg) => {
+		// console.log(msg);
+		io.emit('message', msg);
+	});
+
+	socket.on('disconnect', () => {
+		console.log('out');
+	});
+});
+
+const PORT = 5000 || process.env.PORT;
+server.listen(PORT, () => {
+	console.log(`server running on port ${PORT}`);
+});

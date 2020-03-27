c7ea520dba372fcd159599f21e35fbb86fece521
sishubjoshi <shisub1999@gmail.com>
2020-03-27 12:44:41 +0530

    deploy to heroku failing

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ io.on('connection', (socket) => {
 	});
 });
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
 	console.log(`server running on port ${PORT}`);
 });
